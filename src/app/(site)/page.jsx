2ac7605adde60efa05de7005cc94d0971427f712
TravelFiend <mikegrace274@gmail.com>
2024-12-04 03:48:20 +0800

    changed for Jim to see it

--- src/app/(site)/page.jsx
+++ src/app/(site)/page.jsx
@@ -11,7 +11,7 @@ const Home = () => {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Go Browns</div>
+      <div>Go Browns bitch</div>
     </main>
   );
 };
