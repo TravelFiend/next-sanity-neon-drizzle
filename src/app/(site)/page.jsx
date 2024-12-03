44a73a6693f32d51272591e1a4445f30363d663b
TravelFiend <mikegrace274@gmail.com>
2024-12-04 03:45:11 +0800

    changed for Jim to see it

--- src/app/(site)/page.jsx
+++ src/app/(site)/page.jsx
@@ -11,7 +11,7 @@ const Home = () => {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Husky testing, only Sanity deploy to go</div>
+      <div>Go Browns</div>
     </main>
   );
 };
