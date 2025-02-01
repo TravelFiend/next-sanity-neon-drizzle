add64116046844dfa94099ac56037ea659e3464c
TravelFiend <mikegrace274@gmail.com>
2025-02-01 18:00:55 +0100

    hoping sanity workflow works now dangit

--- sanity.cli.js
+++ sanity.cli.js
@@ -5,6 +5,7 @@ export default defineCliConfig({
     projectId: 'ihpejoz1',
     dataset: 'development'
   },
+  studioHost: 'mjm-fe-dev',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
