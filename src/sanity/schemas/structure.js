8c0a1f06e5438835223d773c7fe13b864d5cf61b
TravelFiend <mikegrace274@gmail.com>
2025-02-09 17:16:53 +0100

    fixed sanity-dev deploy error

--- src/sanity/schemas/structure.js
+++ src/sanity/schemas/structure.js
@@ -1,10 +1,6 @@
-// export const getDefaultDocumentNode = S => {
-//   return S.document().views([
-//     // Gives all documents the JSON preview,
-//     // as well as the default form view
-//     S.view.form()
-//   ]);
-// };
+export const getDefaultDocumentNode = S => {
+  return S.document().views([]);
+};
 
 const structure = S =>
   S.list()
