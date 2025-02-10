46c9ecaeba4498999b60e0f2597c99208c2e5f98
TravelFiend <mikegrace274@gmail.com>
2025-02-10 01:39:16 +0100

    only fetching headingFont since that is all we need for layout logic

--- src/app/(site)/layout.jsx
+++ src/app/(site)/layout.jsx
@@ -8,7 +8,7 @@ import client from '@/sanity/config/client-config';
   regular google fonts but you'll need a different setup for
   the following font variables (i.e. adding a new property:
   `weights: ['400', '700']` to the font object).  These variables
-  are thenadded in `globals.css` under the `@theme inline`
+  are then added in `globals.css` under the `@theme inline`
   directive */
 
 // sans-serif
@@ -35,18 +35,18 @@ export const metadata = {
 };
 
 const RootLayout = async ({ children }) => {
-  const {
-    fonts: { headingFont }
-  } = await client.fetch(`*[_type == "siteSettings"][0]{
+  const { fonts } = await client.fetch(`*[_type == "siteSettings"][0]{
     fonts {
-      headingFont
+      headingFont,
+      bodyFont,
+      captionFont
     }
   }`);
 
   return (
     <html lang="en">
       <body
-        className={` ${headingFont === 'sans-serif' ? rubik.className : rubik.variable} ${headingFont === 'serif' ? ebGaramond.className : ebGaramond.variable} ${headingFont === 'mono' ? martianMono.className : martianMono.variable} `}
+        className={`${fonts.headingFont === 'sans-serif' ? rubik.className : rubik.variable} ${fonts.headingFont === 'serif' ? ebGaramond.className : ebGaramond.variable} ${fonts.headingFont === 'mono' ? martianMono.className : martianMono.variable}`}
       >
         <MainHeader />
         <main>{children}</main>
--- src/app/(site)/page.jsx
+++ src/app/(site)/page.jsx
@@ -2,7 +2,7 @@
 
 const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 font-serif">
       <div>Go Browns bitch ayoo madood</div>
     </main>
   );
