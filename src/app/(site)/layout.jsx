9300f1a200b8ea66365b54e8b2c8b4b97c6999be
TravelFiend <mikegrace274@gmail.com>
2025-02-13 23:49:31 +0100

    added new root layout so there is only one html and body tag

--- src/app/(site)/layout.jsx
+++ src/app/(site)/layout.jsx
@@ -1,63 +1,19 @@
-import '../globals.css';
-import { Rubik, EB_Garamond, Martian_Mono } from 'next/font/google';
 import MainHeader from '@/components/layout/MainHeader';
 import Footer from '@/components/layout/Footer';
-import client from '@/sanity/config/client-config';
-
-/* We're using google variable fonts here.  You can also use
-  regular google fonts but you'll need a different setup for
-  the following font variables (i.e. adding a new property:
-  `weights: ['400', '700']` to the font object) */
-
-// sans-serif
-const rubik = Rubik({
-  subsets: ['latin'],
-  variable: '--font-rubik'
-});
-
-// serif
-const ebGaramond = EB_Garamond({
-  subsets: ['latin'],
-  variable: '--font-garamond'
-});
-
-// mono
-const martianMono = Martian_Mono({
-  subsets: ['latin'],
-  variable: '--font-martian'
-});
 
 export const metadata = {
   title: 'MJM LLC',
   description: 'Music, Art, Technology'
 };
 
-const RootLayout = async ({ children }) => {
-  const {
-    fonts: { headingFont }
-  } = await client.fetch(`*[_type == "siteSettings"][0]{
-    fonts {
-      headingFont
-    }
-  }`);
-
+const siteLayout = async ({ children }) => {
   return (
-    <html lang="en">
-      <body
-        className={` ${rubik.variable} ${ebGaramond.variable} ${martianMono.variable} ${
-          headingFont === 'sans-serif'
-            ? rubik.className
-            : headingFont === 'serif'
-              ? ebGaramond.className
-              : martianMono.className
-        } `}
-      >
-        <MainHeader />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <>
+      <MainHeader />
+      <main>{children}</main>
+      <Footer />
+    </>
   );
 };
 
-export default RootLayout;
+export default siteLayout;
---
+++ src/app/layout.jsx
@@ -0,0 +1,59 @@
+import './globals.css';
+import { Rubik, EB_Garamond, Martian_Mono } from 'next/font/google';
+import client from '@/sanity/config/client-config';
+
+/* We're using google variable fonts here.  You can also use
+  regular google fonts but you'll need a different setup for
+  the following font variables (i.e. adding a new property:
+  `weights: ['400', '700']` to the font object) */
+
+// sans-serif
+const rubik = Rubik({
+  subsets: ['latin'],
+  variable: '--font-rubik'
+});
+
+// serif
+const ebGaramond = EB_Garamond({
+  subsets: ['latin'],
+  variable: '--font-garamond'
+});
+
+// mono
+const martianMono = Martian_Mono({
+  subsets: ['latin'],
+  variable: '--font-martian'
+});
+
+export const metadata = {
+  title: 'E-Commerce-ism',
+  description: 'We doin thangs, Next + Sanity'
+};
+
+const RootLayout = async ({ children }) => {
+  const {
+    fonts: { headingFont }
+  } = await client.fetch(`*[_type == "siteSettings"][0]{
+    fonts {
+      headingFont
+    }
+  }`);
+
+  return (
+    <html lang="en">
+      <body
+        className={` ${rubik.variable} ${ebGaramond.variable} ${martianMono.variable} ${
+          headingFont === 'sans-serif'
+            ? rubik.className
+            : headingFont === 'serif'
+              ? ebGaramond.className
+              : martianMono.className
+        } `}
+      >
+        {children}
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
