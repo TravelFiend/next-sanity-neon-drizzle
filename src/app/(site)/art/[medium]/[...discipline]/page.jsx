979d638668ea26badc9d072ecf47e4279c40ca31
TravelFiend <mikegrace274@gmail.com>
2025-03-16 19:49:14 +0000

    renamed dynamic artist path to correctly read discipline instead, think of it like a subgenre, but for art

--- src/app/(site)/art/[medium]/[...discipline]/page.jsx
+++ src/app/(site)/art/[medium]/[...discipline]/page.jsx
@@ -1,8 +1,8 @@
 import ContentWrapper from '@/components/common/ContentWrapper';
 
-const ArtistPage = async ({ params }) => {
+const DisciplinePage = async ({ params }) => {
   const { discipline } = await params;
   return <ContentWrapper>{discipline}</ContentWrapper>;
 };
 
-export default ArtistPage;
+export default DisciplinePage;
