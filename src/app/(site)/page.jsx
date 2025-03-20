deda44956d39bcc74aa7d472ea8c13cf47f56d92
TravelFiend <mikegrace274@gmail.com>
2025-03-20 19:21:49 +0000

    updated homepage to only error if no data

--- src/app/(site)/page.jsx
+++ src/app/(site)/page.jsx
@@ -3,11 +3,10 @@ import ContentWrapper from '@/components/common/ContentWrapper';
 import ContentBlocks from '@/components/blocks/ContentBlocks';
 
 const Home = async () => {
-  const {
-    data: { title, contentBlocks }
-  } = await getHomepage();
+  const { data } = await getHomepage();
+  const { title, contentBlocks } = data;
 
-  if (!title || !contentBlocks) {
+  if (!data) {
     throw new Error('Sanity homepage data not found');
   }
 
