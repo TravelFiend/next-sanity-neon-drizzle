6e40ae7bdb0be94a425256680ff86816d0a3d787
TravelFiend <mikegrace274@gmail.com>
2025-06-02 01:11:14 +0700

    fallback component instead of throwing error so build does not fail hopefully

--- src/app/(site)/page.jsx
+++ src/app/(site)/page.jsx
@@ -2,8 +2,17 @@ import { getHomepage } from '@/lib/actions/groqQueries/queries/homepage';
 import ContentBlocks from '@/components/blocks/ContentBlocks';
 
 const Home = async () => {
-  const { data } = await getHomepage();
-  const { contentBlocks } = data;
+  const homepage = await getHomepage();
+  if (!homepage) {
+    return (
+      <div className="p-8 text-center">
+        <h1 className="text-2xl font-bold">Welcome to the site!</h1>
+        <p>No Homepage content was returned. Please see error logs.</p>
+      </div>
+    );
+  }
+
+  const { contentBlocks } = homepage.data;
 
   return <ContentBlocks contentBlocks={contentBlocks} />;
 };
