d02725e759f1cbd8f1f24b026db4a5bf15094d3b
TravelFiend <mikegrace274@gmail.com>
2025-06-17 22:00:04 +0700

    workflows updated to include new schema deploy step

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,6 @@
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
     domains: ['cdn.sanity.io'],
     remotePatterns: [{ hostname: `res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/**` }]
