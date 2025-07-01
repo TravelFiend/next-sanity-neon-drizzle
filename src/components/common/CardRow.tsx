993fa003f669bf8ed52114e664b964a21bd4d787
TravelFiend <mikegrace274@gmail.com>
2025-07-01 21:45:38 +0700

    CardRow component typescriptified

--- src/components/common/CardRow.tsx
+++ src/components/common/CardRow.tsx
@@ -1,7 +1,12 @@
 import conditionalClasses from '@/lib/utils/conditionalClasses';
 import CloudinaryImg from './CloudinaryImg';
+import { CarouselBlock } from '@/sanity/types';
 
-const CardRow = ({ cards }) => {
+type CardRowProps = {
+  cards: CarouselBlock['images'];
+};
+
+const CardRow: React.FC<CardRowProps> = ({ cards }) => {
   if (!cards || !Array.isArray(cards) || !cards.length) {
     throw new Error('Invalid cards data provided to CardRow component');
   }
@@ -24,8 +29,8 @@ const CardRow = ({ cards }) => {
           aria-label={`Item ${idx + 1} of ${cards.length}: ${card.altText}`}
         >
           <CloudinaryImg
-            src={card.imageAsset.public_id}
-            alt={card.altText}
+            src={card.imageAsset.public_id!}
+            alt={card.altText!}
             sizes={cards.length === 3 ? '33vw' : '25vw'}
             className="h-full w-full object-cover"
           />
