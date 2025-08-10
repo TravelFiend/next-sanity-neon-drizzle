4e8bce8120fe267e597265a4f1ad72e5fc10a260
TravelFiend <mikegrace274@gmail.com>
2025-08-10 17:03:26 +0800

    added generic loading fallback for fetch calls

---
+++ src/app/(site)/loading.tsx
@@ -0,0 +1,10 @@
+const LoadingPage = () => {
+  return (
+    <section className="m-auto flex min-h-2/3">
+      <p className="py-20">UPDATE ME WITH A BADASS LOADING SPINNER OR SUMFIN</p>
+      <p className="py-20">Loading...</p>
+    </section>
+  );
+};
+
+export default LoadingPage;
