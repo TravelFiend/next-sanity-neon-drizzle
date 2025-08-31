aef2a5ca32ff9f01afc2f9da3b7a94f26efd0050
TravelFiend <mikegrace274@gmail.com>
2025-08-31 22:59:11 +0800

    testing no dotenv package

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,4 @@
-import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
-
-config({ path: '.env.local' });
 
 export const DATABASE_URL = process.env.DATABASE_URL!;
 
--- src/_drizzle/db.ts
+++ src/_drizzle/db.ts
@@ -1,9 +1,6 @@
-import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as schema from './schemas';
-
-config({ path: '.env.local' });
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL
