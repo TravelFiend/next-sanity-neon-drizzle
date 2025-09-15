298ee5c95fe98a0ad52a2c05c6d899c5873b3653
TravelFiend <mikegrace274@gmail.com>
2025-09-15 23:31:35 +0800

    updated drizzle-kit to auto map camelCase to snake_case

---
+++ src/_drizzle/migrations/0000_jittery_joseph.sql
@@ -0,0 +1,41 @@
+CREATE TYPE "public"."address_type" AS ENUM('shipping', 'billing');--> statement-breakpoint
+CREATE TYPE "public"."oauth_providers" AS ENUM('google', 'facebook', 'discord', 'github');--> statement-breakpoint
+CREATE TYPE "public"."role" AS ENUM('customer', 'artist', 'musician');--> statement-breakpoint
+CREATE TABLE "user_addresses" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" uuid NOT NULL,
+	"address_type" "address_type" DEFAULT 'shipping' NOT NULL,
+	"address1" varchar(255) NOT NULL,
+	"address2" varchar(255),
+	"city" varchar(50) NOT NULL,
+	"state" varchar(2) NOT NULL,
+	"zip_code" varchar(15) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "user_oauth_accounts" (
+	"user_id" uuid NOT NULL,
+	"provider" "oauth_providers" NOT NULL,
+	"provider_account_id" text NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "user_oauth_accounts_provider_account_id_provider_pk" PRIMARY KEY("provider_account_id","provider"),
+	CONSTRAINT "user_oauth_accounts_providerAccountId_unique" UNIQUE("provider_account_id")
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"password" varchar(255),
+	"username" varchar(255),
+	"first_name" varchar(255),
+	"last_name" varchar(255),
+	"birthday" date,
+	"role" "role" DEFAULT 'customer' NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+ALTER TABLE "user_addresses" ADD CONSTRAINT "user_addresses_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_oauth_accounts" ADD CONSTRAINT "user_oauth_accounts_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
--- src/_drizzle/schemas/usersDrizzle.ts
+++ src/_drizzle/schemas/usersDrizzle.ts
@@ -15,16 +15,16 @@ import {
 export const rolesEnum = pgEnum('role', ['customer', 'artist', 'musician']);
 
 export const usersTable = pgTable('users', {
-  id: uuid('id').primaryKey().defaultRandom(),
-  email: varchar('email', { length: 255 }).notNull().unique(),
-  password: varchar('password', { length: 255 }),
-  username: varchar('username', { length: 255 }),
-  firstName: varchar('first_name', { length: 255 }),
-  lastName: varchar('last_name', { length: 255 }),
-  birthday: date('birthday'),
+  id: uuid().primaryKey().defaultRandom(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }),
+  username: varchar({ length: 255 }),
+  firstName: varchar({ length: 255 }),
+  lastName: varchar({ length: 255 }),
+  birthday: date(),
   role: rolesEnum().notNull().default('customer'),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at')
+  createdAt: timestamp().notNull().defaultNow(),
+  updatedAt: timestamp()
     .notNull()
     .defaultNow()
     .$onUpdate(() => new Date())
@@ -78,17 +78,17 @@ export const userOAuthAccountRelations = relations(
 export const addressTypeEnum = pgEnum('address_type', ['shipping', 'billing']);
 
 export const userAddressesTable = pgTable('user_addresses', {
-  id: serial('id').primaryKey(),
-  userId: uuid('user_id')
+  id: serial().primaryKey(),
+  userId: uuid()
     .references(() => usersTable.id)
     .notNull(),
-  addressType: addressTypeEnum('address_type').notNull().default('shipping'),
-  address1: varchar('address_1', { length: 255 }).notNull(),
-  address2: varchar('address_2', { length: 255 }),
-  city: varchar('city', { length: 50 }).notNull(),
-  state: varchar('state', { length: 2 }).notNull(),
-  zipCode: varchar('zip_code', { length: 15 }).notNull(),
-  createdAt: timestamp('created_at').notNull().defaultNow()
+  addressType: addressTypeEnum().notNull().default('shipping'),
+  address1: varchar({ length: 255 }).notNull(),
+  address2: varchar({ length: 255 }),
+  city: varchar({ length: 50 }).notNull(),
+  state: varchar({ length: 2 }).notNull(),
+  zipCode: varchar({ length: 15 }).notNull(),
+  createdAt: timestamp().notNull().defaultNow()
 });
 
 export type InsertUserAddress = typeof userAddressesTable.$inferInsert;
