430db4ca4d126c514e7624ab904deebc8f3a402c
TravelFiend <mikegrace274@gmail.com>
2026-03-06 18:24:47 +0800

    added sanity-plugin-cloudinary and react-refractor to transpilePackages option in next config

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,16 @@ const nextConfig: NextConfig = {
         pathname: `/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/**`
       }
     ]
-  }
+  },
+  serverExternalPackages: [
+    'argon2',
+    'pg'
+  ],
+  transpilePackages: [
+    '@sanity/ui',
+    'sanity-plugin-cloudinary',
+    'react-refractor'
+  ]
 };
 
 export default nextConfig;
